f6016eb3460f166a00e34fd2366b74ac005056ce
humblots <shumblot25@gmail.com>
2024-02-02 09:08:11 +0100

    initial commit

---
+++ Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by HUMBLOT Stéphane on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
